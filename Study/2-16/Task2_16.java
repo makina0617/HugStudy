1cef52b11bdb45cf16f2d3cc46fe7ece832216c0
makina0617 <f.nico.m.moon@gmail.com>
2024-01-07 23:55:06 +0900

    2-16

--- Study/2-16/Task2_16.java
+++ Study/2-16/Task2_16.java
@@ -15,8 +15,8 @@ public class Task2_16 {
 
         // ③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         // 要素が奇数の場合には「nは奇数です。」と出力されます。
-        for (int i = 0; i < arr.length; i++) {
-
+        for (int num = 0; num < arr.length; num++) {
+            System.out.println(isOdd(num));
         }
 
     }
