cdf7be0ae0c36ec35be3a349b8692dc34fecb5a9
makina0617 <f.nico.m.moon@gmail.com>
2024-01-20 18:31:47 +0900

    2-18

--- Study/2-18/Task2_18.java
+++ Study/2-18/Task2_18.java
@@ -6,9 +6,9 @@ public class Task2_18 {
         person.setAge(23);
         person.setAddles("東京都");
         // 問③ 設定した名前・年齢・住所をカプセル化されたgetterで呼び出してして下さい。
-        person.getName();
-        person.getAge();
-        person.getAddles();
+        System.out.println("名前は、" + person.getName() + "です。");
+        System.out.println("年齢は、" + person.getAge() + "才です。");
+        System.out.println("住所は、" + person.getAddles() + "です。");
     }
 }
 
@@ -18,30 +18,27 @@ class Person {
     private String addles;
 
     // 問① 上記変数に従って、getterとsetterを設定して下さい。
-    public void setName(String n) {
-        this.name = n;
-        System.out.println("住所は、" + this.name + "です。");
-    }
-
-    public void setAge(int a) {
-        this.age = a;
-        System.out.println("年齢は、" + this.age + "才です。");
-    }
-
-    public void setAddles(String ad) {
-        this.addles = ad;
-        System.out.println("住所は、" + this.addles + "です。");
-    }
-
     public String getName() {
-        return name;
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
-        return age;
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public String getAddles() {
         return addles;
+    }
+
+    public void setAddles(String addles) {
+        this.addles = addles;
     }
 }
